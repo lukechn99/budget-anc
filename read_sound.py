1fe1c2ec2cb36189222ef131b9a01842315ec710
lukechn99 <47294899+lukechn99@users.noreply.github.com>
2021-10-26 15:23:21 -0500

    added resource

--- read_sound.py
+++ read_sound.py
@@ -1,6 +1,8 @@
 import sounddevice as sd
 import numpy as np
 
+# https://python-sounddevice.readthedocs.io/en/0.3.3/examples.html#plot-microphone-signal-s-in-real-time
+
 def print_sound(indata, outdata, frames, time, status):
     volume_norm = np.linalg.norm(indata)*10
     print ("|" * int(volume_norm))
