4065903b49560d1190c64a37db47cee8233be55c
lukechn99 <47294899+lukechn99@users.noreply.github.com>
2021-11-03 00:17:27 -0500

    Update read_sound.py

--- read_sound.py
+++ read_sound.py
@@ -2,6 +2,7 @@ import sounddevice as sd
 import numpy as np
 
 # https://python-sounddevice.readthedocs.io/en/0.3.3/examples.html#plot-microphone-signal-s-in-real-time
+# Set up sound for WSL https://research.wmz.ninja/articles/2017/11/setting-up-wsl-with-graphics-and-audio.html
 
 def print_sound(indata, outdata, frames, time, status):
     volume_norm = np.linalg.norm(indata)*10
